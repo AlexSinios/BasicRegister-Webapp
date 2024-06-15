e7869da40a213144735c404f6809e3d4d59587ef
AlexSinios <55320895+AlexSinios@users.noreply.github.com>
2024-06-15 14:41:58 +0300

    Update app.py

--- AE_serious/app.py
+++ AE_serious/app.py
@@ -32,6 +32,13 @@ def index():
 def signup():
     """Register user"""
 
+    if session.get("id"):
+        flash("To create a new account, log out of your current one")
+        return error(4)
+    
+    # Kαθαρισμός του session
+    session.clear()
+
     # Confirm that POST has been used as opposed to GET
     if request.method == "POST":
 
@@ -42,20 +49,24 @@ def signup():
         # Make sure a username was provided
         if not username:
             flash("Missing username")
-            return apology(1)
+            return error(1)
 
         # Make sure a password was provided
         elif not password:
             flash("Missing password")
-            return apology(1)
+            return error(1)
         
         elif not pconfirm == password:
             flash("Passwords don't match, try again")
-            return apology(1)
+            return error(1)
+        
+        if len(password) < 6:
+            flash("Password must be at least 6 characters long")
+            return error(1)
 
         # # Make sure the passwords match
         # elif not request.form.get("password") == request.form.get("pword2"):
-        #     return render_template("apology.html")
+        #     return render_template("error.html")
 
         # Hash the password for security reasons
         hash = generate_password_hash(password)
@@ -64,7 +75,7 @@ def signup():
         search = cur.execute("SELECT 1 FROM users WHERE username = ?", (username, ))
         if search.fetchone():
             flash("Username already exists, try another one")
-            return apology(1)
+            return error(1)
         
         # Insert the new users in the SQL database, hashing the password
         cur.execute("INSERT INTO users (username, hash) VALUES(?, ?)", (username, hash))
@@ -86,6 +97,10 @@ def signup():
 def login():
     """Log user in"""
 
+    if session.get("id"):
+        flash("To create a new account, log out of your current one")
+        return error(4)
+    
     # Kαθαρισμός του session
     session.clear()
 
@@ -98,25 +113,25 @@ def login():
         # Ensure username was submitted
         if not username:
             flash("Must provide username")
-            return apology(2)
+            return error(2)
 
         # Ensure password was submitted
         elif not password:
             flash("Must provide password")
-            return apology(2)
+            return error(2)
 
         # Query database for username
         res = cur.execute("SELECT * FROM users WHERE username = ?", (username,))
         pwd = res.fetchone()
         if pwd == None:
             flash(f"Invalid username")
-            return apology(2)
+            return error(2)
 
         # Ensure username exists and password is correct
         if not check_password_hash(pwd[2], password):
             flash(f"Invalid credentials: {check_password_hash(pwd[2], password)}")
-            return apology(2)
-            #return redirect(url_for('apology', action = "Invalid credentials"))
+            return error(2)
+            #return redirect(url_for('error', action = "Invalid credentials"))
         # Remember which user has logged in
         session["id"] = pwd[0]
 
@@ -148,11 +163,62 @@ def deleteacc():
     return redirect("/")
 
 
+@app.route("/changepwd", methods=["GET", "POST"])
+def changepwd():    
+
+    # Confirm that POST has been used as opposed to GET
+    if request.method == "POST":
+
+        user_id = session.get("id")
+
+        if user_id:   
+            oldpass = request.form.get("oldpass").strip()
+            newpass = request.form.get("newpass").strip()
+            pconfirm = request.form.get("pconfirm").strip()
+
+            # Make sure a username was provided
+            if not oldpass:
+                flash("Missing old password")
+                return error(5)
+
+            # Make sure a password was provided
+            elif not newpass:
+                flash("Missing new password")
+                return error(5)
+            
+            elif not pconfirm == newpass:
+                flash("Passwords don't match, try again")
+                return error(5)
+            
+            if len(newpass) < 6:
+                flash("Password must be at least 6 characters long")
+                return error(5)
+            
+            # Query database for user id
+            res = cur.execute("SELECT hash FROM users WHERE id = ?", (user_id,))
+            verif = res.fetchone()
+            if verif == None:
+                return error(5)
+            
+            if not check_password_hash(verif[0], oldpass):
+                flash("Current password entered was invalid")
+                return error(5)
+            
+            cur.execute("UPDATE users SET hash = ? WHERE id = ?", (generate_password_hash(newpass), user_id))
+            con.commit()
+
+            flash("You have successfully updated your password, redirecting to main page...")
+        return redirect("/")
+    
+    else:
+        return render_template("changepwd.html")
+
+
 def errorhandler(e):
     """Handle error"""
     if not isinstance(e, HTTPException):
         e = InternalServerError()
-    return apology(3, e.name, e.code)
+    return error(3, e.name, e.code)
 
 
 # Listen for errors
@@ -160,14 +226,18 @@ for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
 
 
-def apology(source, name="", code=200):
+def error(source, name="", code=200):
     if source == 1:
         action = "while registering your account"
     elif source == 2:
         action = "while logging into your account"
+    elif source == 4:
+        action = ": You are trying to sign up/log in whilst still in an active session. Log out of your current account first. Then"
+    elif source == 5:
+        action = "while changing your password"
     else: 
         action = f"HTTP CODE {code}: '{name}'"
-    return render_template("apology.html", action=action)
+    return render_template("error.html", action=action)
 
 
 if __name__=="__main__":
